839a56e3a95c2e2377c8e5014bbc33471781d612
Knox-764 <80540449+siiine-764@users.noreply.github.com>
2023-05-14 22:28:54 +0100

    Update is_power_of_2.c

--- level2/is_power_of_2/is_power_of_2.c
+++ level2/is_power_of_2/is_power_of_2.c
@@ -1,13 +1,22 @@
-int is_power_of_2(unsigned int n) {
+// int is_power_of_2(unsigned int n) {
+//     if (n == 0)
+//         return 0;
+//     while (n != 1)
+//     {
+//         if (n % 2 != 0)
+//             return 0;
+//         n /= 2;
+//     }
+//     return 1;
+// }
+
+int	    is_power_of_2(unsigned int n)
+{
     if (n == 0)
         return 0;
-    while (n != 1)
-    {
-        if (n % 2 != 0)
-            return 0;
+    while (n % 2 == 0)
         n /= 2;
-    }
-    return 1;
+    return n == 1;
 }
 
 #include <stdio.h>
