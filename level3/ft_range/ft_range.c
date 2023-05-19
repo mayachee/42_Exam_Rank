1a64bd87da7a017f1f7c1cd437b469f5941f0394
Knox-764 <80540449+siiine-764@users.noreply.github.com>
2023-05-19 20:04:47 +0000

    Update ft_range

--- level3/ft_range/ft_range.c
+++ level3/ft_range/ft_range.c
@@ -2,50 +2,28 @@
 
 int     *ft_range(int start, int end)
 {
-    int m = 0;
-    if (start > end)
-        ft_range(end, start);
-    int len;
-    int i = 0;
-    len = end - start;
-    if (len)
-    {
-        int *nbr = malloc(sizeof(int *) * len);
-        if (!nbr)
-            return NULL;
-        while (start <= end)
+	int i = 0;
+  	int len = abs((end - start)) + 1;
+  	int *res; 
+  	res = (int *)malloc(sizeof(int) * len);
+  
+  	while (i < len)
+  	{
+    	  if (start < end)
+    	  {
+		  res[i] = start;
+		  start++;
+    		  i++;
+          }
+        else
         {
-            nbr[i] = start;
-            i++;
-            start++;   
+                res[i] = start;
+                start--;
+                i++;
         }
-        return nbr;
-    }
-    return NULL;
+  	}
+  	return (res);
 }
-
-// int     *ft_range(int start, int end)
-// {
-//     int len = end - start + 1;
-//     int i = 0;
-//     int *nbr;
-//     if (start > end)
-//         return (ft_range(end, start));
-//     nbr = (int *)malloc(sizeof(int) * len);
-//     if (nbr)
-//     {
-//         while (start <= end)
-//         {
-//             nbr[i] = start;
-//             i++;
-//             start++;
-//         }
-//     }
-//     return nbr;
-// }
-
-
-
 
 #include <stdio.h>
 int     main(void)
