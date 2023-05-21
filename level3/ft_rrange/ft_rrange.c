6c858c0025f168b9e82deb9a962c3beeb1a1fbb1
Knox-764 <80540449+siiine-764@users.noreply.github.com>
2023-05-21 18:37:51 +0100

    Update ft_rrange.c

--- level3/ft_rrange/ft_rrange.c
+++ level3/ft_rrange/ft_rrange.c
@@ -1,25 +1,49 @@
 #include <stdlib.h>
 
-int *ft_rrange(int start, int end)
-{
-	int *range;
-	int i = 0;
-	int n = end - start + 1;
+// int *ft_rrange(int start, int end)
+// {
+// 	int *range;
+// 	int i = 0;
+// 	int n = end - start + 1;
 
-	if (start > end)
-		return (ft_rrange(end, start));
-	range = (int *)malloc(sizeof(int) * n);
-	if (range)
-	{
-		while (i < n)
-		{
-			range[i] = end;
-			end--;
-			i++;
-		}
-	}
-	return (range);
+// 	if (start > end)
+// 		return (ft_rrange(end, start));
+// 	range = (int *)malloc(sizeof(int) * n);
+// 	if (range)
+// 	{
+// 		while (i < n)
+// 		{
+// 			range[i] = end;
+// 			end--;
+// 			i++;
+// 		}
+// 	}
+// 	return (range);
+// }
+
+int     *ft_rrange(int start, int end)
+{
+    int len = abs(end - start) +1;
+    int i  = 0;
+    int *str = (int *)malloc(sizeof(str) * len);
+    if (!str)
+        return NULL;
+    while (i < len) {
+        if (start > end)
+        {
+            str[i] = end;
+            end++;
+            i++;
+        }
+        else {
+            str[i] = end;
+            end--;
+            i++;
+        }
+    }
+    return str;
 }
+
 // #include <stdio.h>
 // int main()
 // {
